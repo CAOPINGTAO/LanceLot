322838a503d0bb5491cbddcc778edf4c7a87a526
lancelot <1195051604@qq.com>
2016-05-10 13:57:57 +0800

    新增\Think\UploadFile类，以实现上传的同时对图片进行缩略的功能

--- Application/Admin/Controller/MovieController.class.php
+++ Application/Admin/Controller/MovieController.class.php
@@ -25,7 +25,299 @@ class MovieController extends CommonController{
 	//添加影片基本信息
 	public function insert(){
 		//文件上传
-		$upload = new \Think\Upload();
-		
+		$upload = new \Think\UploadFile();
+		$upload->maxSize = 3145728;
+
+        $upload->thumb = true;
+        $upload->thumbMaxWidth = '50,70,100,140';
+        $upload->thumbMaxHeight = '50,100,150,200';
+        $upload->thumbPrefix = 'a_,b_,c_,d_';
+
+        $upload->allowExts = array('jpg', 'gif', 'png', 'jpeg');
+        $upload->savePath = './Uploads/Movie/Cover/'; //设置附件上传目录
+
+        if (!$upload->upload()) {
+        	//上传错误提示
+        	 $this->error($upload->getErrorMsg());
+        } else {
+        	//上传成功，获取上传文件信息
+        	$info = $upload->getUploadFileInfo();
+        }
+
+        $_POST['showtime'] = strtotime($_POST['showtime']);
+        $_POST['picname'] = $info[0]['savename'];
+        $_POST['addtime'] = time();
+
+        //添加影片基本信息
+        parent::insert();
 	}
+
+	/**
+	 *	删除影片时
+	 *	1. 删除影片的基本信息;
+	 *	2. 删除影片的封面图片及相关缩略图
+	 *	3. 删除与影片相关联的类别信息
+	 **/
+	public function delete(){
+
+		$map['id'] = $_GET['id'];
+		$movie = M('movie')->where($map)->find();
+		$picname = $movie['picname'];
+
+		// 2. 删除影片的封面图片及相关缩略图
+		$path = "./Uploads/Movie/Cover";
+		unlink($path."/".$picname);
+		unlink($path."/a_".$picname);
+		unlink($path."/b_".$picname);
+		unlink($path."/c_".$picname);
+		unlink($path."/d_".$picname);
+
+		// 1. 删除影片基本信息
+		parent::delete();
+	}
+
+	/*
+	 *	更新影片基本信息时
+	 * 	1. 判断是否有图片上传，如果有图片上传，则在新图片上传成功后删除原有图片
+	 *	2. 如果没有图片上传，则更新其他信息
+	 */
+	public function update(){
+
+		if($_FILES['picname']['error'] == 4){
+			// 没有图片上传时
+			$_POST['picname'] = $_POST['pname'];
+
+			$_POST['showtime'] = strtotime($_POST['showtime']);
+			parent::update();
+		}
+
+		// 删除影片的原封面图片及相关缩略图
+		$picname = $_POST['pname'];
+		$_POST['showtime'] = strtotime($_POST['showtime']);
+
+		$path = "./Uploads/Movie/Cover";
+		unlink($path."/".$picname);
+		unlink($path."/a_".$picname);
+		unlink($path."/b_".$picname);
+		unlink($path."/c_".$picname);
+		unlink($path."/d_".$picname);
+
+
+		// 上传新图片
+		$upload = new \Think\UploadFile();// 实例化上传类
+		$upload->maxSize  = 3145728 ;// 设置附件上传大小
+
+		$upload->thumb = true;
+		$upload->thumbMaxWidth  = '50,70,100,140';
+		$upload->thumbMaxHeight = '50,100,150,200';
+		$upload->thumbPrefix = "a_,b_,c_,d_";
+
+		$upload->allowExts  = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+		$upload->savePath =  './Uploads/Movie/Cover/';// 设置附件上传目录
+
+		if(!$upload->upload()) {
+			// 上传错误提示错误信息
+			$this->error($upload->getErrorMsg());
+		}else{
+			// 上传成功 获取上传文件信息
+			$info =  $upload->getUploadFileInfo();
+		}
+
+		$_POST['picname'] = $info[0]['savename'];
+		parent::update();
+	}
+
+	// 查看详情
+	public function content(){
+		$map['id'] = $_GET['id'];
+		$vo = M('movie')->field('content')->where($map)->find();
+		$this->assign('content', $vo['content']);
+		$this->display();
+	}
+
+	// 设置分类视图
+	public function setMType(){
+
+		// 获取影片ID
+		$movid = $_GET['id'];
+		$mt = M('movie_type')->where("fid={$movid}")->select();
+	
+		if($mt && count($mt)>0){
+			$this->error('此影片已设置分类信息！');
+			exit();
+		}
+		$type = M('type')->select();
+
+		$mtype = array('类型' => array(), '国家/地区' => array(), '年代' => array());
+		foreach ($type as $vo) {
+			if ($vo['fid'] == 1) {
+				$mtype1['类型'][] = $vo;			
+			}
+		}
+
+		foreach ($type as $vo) {
+			if ($vo['fid'] == 2) {
+				$mtype2['国家/地区'][] = $vo;
+			}elseif ($vo['fid'] == 3){
+				$mtype2['年代'][] = $vo;
+			}
+		}
+
+		$this->assign('movie_id', $movid);
+		$this->assign('type1', $mtype1);
+		$this->assign('type2', $mtype2);
+		
+		$this->display("setMType");
+	}
+
+	// 设置分类表单处理
+	public function setTypeHandle(){
+
+		$fid = array_shift($_POST);	
+		$movie_type = M('movie_type');
+		$movie = M('Movie')->where($fid)->find();
+
+		foreach ($_POST as $value) {
+			if (is_array($value)) {
+				foreach ($value as $v) {
+					$data['fid'] = $fid;
+					$data['tid'] = $v;
+					if (!$movie_type->data($data)->add()) {
+						$this->error("设置失败！");
+					}	
+				}
+			}else{
+				$data['fid'] = $fid;
+				$data['tid'] = $value;
+				if (!$movie_type->data($data)->add()) {
+					$this->error("设置失败！");
+				}	
+			}
+		}
+		
+		$this->success("设置成功！");
+	}
+
+	// 查看影片分类
+	public function showType(){
+
+		$map['id'] = $_GET['id'];
+		$movie = M('movie')->field('id,filmname,addtime')->where($map)->find();
+
+		$mt['fid'] = $movie['id'];
+		$mtype = M('movie_type')->field('tid')->where($mt)->select();
+		
+		foreach ($mtype as $vo) {
+			$t['id'] = $vo['tid'];
+			$type[] = M('type')->field('fid,typename')->where($t)->find();
+		}
+		$movie['type'] = $type;
+
+		$this->assign('mt', $movie);
+		$this->display();
+	}
+
+	// 修改分类视图
+	public function edittype(){
+		// 获取影片ID
+		$movid = $_GET['id'];
+		$type = M('type')->select();
+
+		$mtype = array('类型' => array(), '国家/地区' => array(), '年代' => array());
+		foreach ($type as $vo) {
+			if ($vo['fid'] == 1) {
+				$mtype1['类型'][] = $vo;			
+			}
+		}
+
+		foreach ($type as $vo) {
+			if ($vo['fid'] == 2) {
+				$mtype2['国家/地区'][] = $vo;
+			}elseif ($vo['fid'] == 3){
+				$mtype2['年代'][] = $vo;
+			}
+		}
+
+		$this->assign('movie_id', $movid);
+		$this->assign('type1', $mtype1);
+		$this->assign('type2', $mtype2);
+		$this->display();
+	}
+
+	// 修改分类表单处理
+	public function editTypeHandle(){
+		// 删除影片原有类型
+		// var_dump($_POST); 
+		M('movie_type')->where("fid = {$_POST['movid']}")->delete();
+		
+		// 为影片添加新类型
+		$fid = array_shift($_POST);
+		$movie_type = M('movie_type');
+		$movie = M('Movie')->where($fid)->find();
+
+		foreach ($_POST as $value) {
+			if (is_array($value)) {
+				foreach ($value as $v) {
+					$data['fid'] = $fid;
+					$data['tid'] = $v;
+					if (!$movie_type->data($data)->add()) {
+						$this->error("设置失败！");
+					}	
+				}
+			}else{
+				$data['fid'] = $fid;
+				$data['tid'] = $value;
+				if (!$movie_type->data($data)->add()) {
+					$this->error("设置失败！");
+				}	
+			}
+		}
+		$this->success("设置成功！");
+	}
+
+	// 上传剧照视图
+	public function uploads(){
+		$movid = $_GET['id'];
+		$this->assign('movid', $movid);
+		$this->display();
+	}
+
+	// 上传剧照处理
+	public function uploadsHandle(){
+
+		//实例化上传类
+		$upload = new \Think\UploadFile();
+		$upload->maxSize  = 3145728 ;// 设置附件上传大小
+		$upload->allowExts  = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+		$upload->savePath =  './Uploads/Movie/mPhotos/';// 设置附件上传目录
+
+		$upload->thumb = true;
+		$upload->thumbMaxWidth  = '100';
+		$upload->thumbMaxHeight = '100';
+		$upload->thumbPrefix = "m_";
+
+		if(!$upload->upload()) {// 上传错误提示错误信息
+			$this->error($upload->getErrorMsg());
+		}else{// 上传成功 获取上传文件信息
+			$info =  $upload->getUploadFileInfo();
+		}
+
+		//添加剧照信息
+		$pic = M('filmpic');
+		foreach ($info as $val) {
+			$data['fid'] = $_POST['id'];
+			$data['picname'] = $val['savename'];
+
+			if (!$pic->add($data)) {
+				$this->error('添加剧照失败！');
+			}
+		}
+
+		//修改影片剧照是否设置字段
+		M('Movie')->where("id={$_POST['id']}")->setField('hasphotos', 1);
+		$this->success('成功设置影片剧照信息！');
+	}
+
+	//修改剧照视图
+	
 }
---
+++ Application/Runtime/Cache/Admin/751bfc1b614f88135413bfdacdc4f115.php
@@ -0,0 +1,168 @@
+<?php if (!defined('THINK_PATH')) exit();?><form id="pagerForm" action="/LanceLot/admin.php/Movie/index" method="post">
+	<input type="hidden" name="pageNum" value="<?php echo ((isset($currentPage) && ($currentPage !== ""))?($currentPage):'1'); ?>" />
+	<input type="hidden" name="numPerPage" value="<?php echo ($numPerPage); ?>" />
+	<input type="hidden" name="_order" value="<?php echo ($_REQUEST['_order']); ?>" />
+	<input type="hidden" name="_sort" value="<?php echo ($_REQUEST['_sort']); ?>" />
+</form>
+
+<div class="pageHeader">
+	<form rel="pagerForm" onsubmit="return navTabSearch(this);" action="" method="post">
+		<div class="searchBar">
+			<table class="searchContent">
+				<tr>
+					<td>
+						<label>搜索电影：</label>
+						<input type="text" name="keyword" value="<?php echo ($_REQUEST['keyword']); ?>" />[关键字：影片名，影片别名]
+					</td>
+					<td>
+						<select class="combox" name="status">
+							<option value="">--影片状态--</option>
+							<option value="0">新添加</option>
+							<option value="1">显示</option>
+							<option value="2">幻灯片</option>
+						</select>
+					</td>
+					<td>
+						<div class="buttonActive"><div class="buttonContent"><button type="submit">检索</button></div></div>
+					</td>	
+				</tr>
+			</table>
+		</div>
+	</form>
+</div>
+
+<div class="pageContent">
+	<div class="panelBar">
+		<ul class="toolBar">
+			<li><a class="add" href="/LanceLot/admin.php/Movie/add" target="navTab" rel="user_msg" title="添加影片"><span>添加影片</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="delete" href="/LanceLot/admin.php/Movie/delete/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="ajaxTodo" title="确定要删除吗?"><span>删除</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/edit/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="navTab"><span>修改</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/setmtype/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="dialog"><span>设置分类</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/edittype/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="dialog"><span>修改分类</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/uploads/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="dialog"><span>上传剧照</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/editUploads/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="dialog"><span>修改剧照</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/setActors/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="dialog"><span>设置演员信息</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/editactors/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="dialog"><span>修改演员信息</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="edit" href="/LanceLot/admin.php/Movie/editstatus/id/<?php echo (C("TMPL_L_DELIM")); ?>item_id<?php echo (C("TMPL_R_DELIM")); ?>" target="dialog"><span>修改状态</span></a></li>
+
+			<li class="line">line</li>
+			<li><a class="icon" href="javascript:navTabPageBreak()"><span>刷新</span></a></li>
+		</ul>
+	</div>
+
+	<div id="w_list_print">
+		<table class="list" width="100%" targetType="navTab" asc="asc" desc="desc" layoutH="116">
+			<thead>
+				<tr>
+					<?php if($_REQUEST['_order']== 'id'): ?><th orderField="id" class="<?php echo ($_REQUEST['_sort']); ?>">ID</th>
+					<?php else: ?>
+					<th orderField="id">ID</th><?php endif; ?>
+					<th>影片封面</th>
+					<?php if($_REQUEST['_order']== 'filmname'): ?><th orderField="filmname" class="<?php echo ($_REQUEST['_sort']); ?>">影片名</th>
+					<?php else: ?>
+					<th orderField="filmname">影片名</th><?php endif; ?>
+					<th>影片别名</th>
+					<th>导演</th>
+					<th>编剧</th>
+					<?php if($_REQUEST['_order']== 'nation'): ?><th orderField="nation" class="<?php echo ($_REQUEST['_sort']); ?>">制片国家/地区</th>
+					<?php else: ?>
+					<th orderField="nation">制片国家/地区</th><?php endif; ?>
+					<th>语言</th>
+
+					<?php if($_REQUEST['_order']== 'showtime'): ?><th orderField="showtime" class="<?php echo ($_REQUEST['_sort']); ?>">上映时间</th>
+					<?php else: ?>
+					<th orderField="showtime">上映时间</th><?php endif; ?>
+					<?php if($_REQUEST['_order']== 'addtime'): ?><th orderField="addtime" class="<?php echo ($_REQUEST['_sort']); ?>">添加时间</th>
+					<?php else: ?>
+					<th orderField="addtime">添加时间</th><?php endif; ?>
+					<?php if($_REQUEST['_order']== 'minutes'): ?><th orderField="minutes" class="<?php echo ($_REQUEST['_sort']); ?>">片长</th>
+					<?php else: ?>
+					<th orderField="minutes">片长</th><?php endif; ?>
+					<th>影片简介</th>
+					<?php if($_REQUEST['_order']== 'status'): ?><th orderField="status" class="<?php echo ($_REQUEST['_sort']); ?>">状态</th>
+					<?php else: ?>
+					<th orderField="status">状态</th><?php endif; ?>
+					<th>设置演员信息</th>
+					<th>上传剧照</th>
+					<th>影片分类</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><tr target="item_id" rel="<?php echo ($vo["id"]); ?>">
+						<td><?php echo ($vo["id"]); ?></td>
+						<td>
+							<img src="/LanceLot/Uploads/Movie/Cover/a_<?php echo ($vo["picname"]); ?>" />
+						</td>
+						<td><?php echo ($vo["filmname"]); ?></td>
+						<td><?php echo ($vo["petname"]); ?></td>
+						<td><?php echo ($vo["director"]); ?></td>
+						<td><?php echo ($vo["editor"]); ?></td>
+						<td><?php echo ($vo["nation"]); ?></td>
+						<td><?php echo ($vo["language"]); ?></td>
+						<td><?php echo (date("Y-m-d",$vo["showtime"])); ?></td>
+						<td><?php echo (date("Y-m-d H:i:s",$vo["addtime"])); ?></td>
+						<td><?php echo ($vo["minutes"]); ?></td>
+						<td><a style="color:blue;" href="/LanceLot/admin.php/Movie/content/id/<?php echo ($vo["id"]); ?>" target="dialog" width="600" height="480" rel="listmovie" title="查看简介">查看简介</a></td>
+						<td>
+							<?php if($vo["status"] == 0): ?>新添加
+							<?php elseif($vo["status"] == 1): ?>显示
+							<?php else: ?>幻灯片<?php endif; ?>
+						</td>
+						<td>
+							<a style="color:blue;" target="dialog" rel="setActors" href="/LanceLot/admin.php/Movie/showActors/id/<?php echo ($vo["id"]); ?>">产看演员信息</a>
+						</td>
+						<td>
+							<a style="color:blue;" target="dialog" rel="showPhotos" href="/LanceLot/admin.php/Movie/showPhotos/id/<?php echo ($vo["id"]); ?>">查看剧照信息</a>
+						</td>
+						<td>
+							<a style="color;blue;" target="dialog" rel="showMType" href="/LanceLot/admin.php/Movie/showType/id/<?php echo ($vo["id"]); ?>">查看分类信息</a>
+						</td>
+					</tr><?php endforeach; endif; else: echo "" ;endif; ?>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="panelBar">
+		<div class="pages">
+            <span>显示</span>
+            <select class="combox" name="numPerPage" onchange="navTabPageBreak(<?php echo (C("TMPL_L_DELIM")); ?>numPerPage:this.value<?php echo (C("TMPL_R_DELIM")); ?>)">
+               <?php if($numPerPage == 10): ?><option value="10" selected>10</option>
+               <?php else: ?>
+               <option value="10">10</option><?php endif; ?>
+               <?php if($numPerPage == 15): ?><option value="15" selected>15</option>
+               <?php else: ?>
+               <option value="15">15</option><?php endif; ?>
+               <?php if($numPerPage == 20): ?><option value="20" selected>20</option>
+               <?php else: ?>
+               <option value="20">20</option><?php endif; ?>
+               <?php if($numPerPage == 25): ?><option value="25" selected>25</option>
+               <?php else: ?>
+               <option value="25">25</option><?php endif; ?>
+               <?php if($numPerPage == 30): ?><option value="30" selected>30</option>
+               <?php else: ?>
+               <option value="30">30</option><?php endif; ?>
+            </select>
+            <span>共<?php echo ($totalCount); ?>条</span>
+        </div> 
+
+        <div class="pagination" targetType="navTab" totalCount="<?php echo ($totalCount); ?>" numPerPage="<?php echo ($numPerPage); ?>" pageNumshown="10" currentPage="<?php echo ($currentPage); ?>">
+	</div>
+</div>
