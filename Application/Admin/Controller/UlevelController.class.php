0f32e2b5c8b3d8e2ab9c2bde6f9add91f431b962
lancelot <1195051604@qq.com>
2016-05-11 19:41:49 +0800

    用户管理-->用户等级管理

---
+++ Application/Admin/Controller/UlevelController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 前台用户等级管理
+ * User: Lancelot
+ * Date: 2016/5/11
+ * Time: 19:07
+ */
+namespace Admin\Controller;
+use Admin\Controller\CommonController;
+
+class UlevelController extends CommonController{
+
+	
+}
