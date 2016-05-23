28505467bc66497f84c9324830df0eabe5ec08d2
lancelot <1195051604@qq.com>
2016-05-24 00:27:44 +0800

    配置登录用户常量

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -5,6 +5,13 @@ return array(
     //播放器路径
     "VIDEO_PLAY_PATH"   =>  "http://localhost:8080__ROOT__",
 
+    //登录用户常量配置
+	"USER_AUTH_KEY"			=>"loginuser",
+
+	//自定义跳转页面
+	'TMPL_ACTION_SUCCESS' => 'Public:jump',
+	'TMPL_ACTION_ERROR' => 'Public:jump',
+
     //分页数量
     "PAGE_COUNT_ONE"    => 10,
     "PAGE_COUNT_TWO"    => 0,
